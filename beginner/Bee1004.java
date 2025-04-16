53d7cd8599d26895eaf7d6dc481ab9acffc23cb1
Marcos Vinícius Nunes <imarcosvinicius@outlook.com>
2025-04-16 07:49:11 -0300

    Simple product

---
+++ beginner/Bee1004.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Bee1004 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int num1 = sc.nextInt();
+        int num2 = sc.nextInt();
+
+        int prod = num1 * num2;
+
+        System.out.println("PROD = " + prod);
+
+        sc.close();
+    }
+}
