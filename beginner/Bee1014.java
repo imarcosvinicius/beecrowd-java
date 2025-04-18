ad382a56ac50da7392ea0ead7e94db59a506462d
Marcos Vinícius Nunes <imarcosvinicius@outlook.com>
2025-04-18 19:01:38 -0300

    Average car consumption

---
+++ beginner/Bee1014.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+import java.util.Locale;
+
+public class Bee104 {
+
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        int distance = sc.nextInt();
+        double fuel = sc.nextDouble();
+
+        double average = distance / fuel;
+
+        System.out.printf("%.3f km/l%n", average);
+
+        sc.close();
+    }
+}
