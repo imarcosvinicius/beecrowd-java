fe98760c2d08a762cb8956461898ccb00beb6d93
Marcos Vinícius Nunes <imarcosvinicius@outlook.com>
2025-04-29 07:40:40 -0300

    refactor code

--- beginner/Bee1001.java
+++ beginner/Bee1001.java
@@ -3,12 +3,16 @@ import java.util.Scanner;
 public class Bee1001 {
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        int result = a + b;
+        try (Scanner sc = new Scanner(System.in)) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            int result = sum(a, b);
+            System.out.println("X = " + result);
+        }
+    }
 
-        System.out.println("X = " + result);
+    private static int sum(int x, int y) {
+        return x + y;
     }
 
 }
--- beginner/Bee1003.java
+++ beginner/Bee1003.java
@@ -3,12 +3,17 @@ import java.util.Scanner;
 public class Bee1003 {
 
     public static void main(String [] args) {
-        Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        int soma = a + b;
+        try (Scanner sc = new Scanner(System.in)) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            int result = sum(a, b);
+    
+            System.out.println("SOMA = " + result);
+        }
+    }
 
-        System.out.println("SOMA = " + soma);
+    private static int sum(int x, int y) {
+        return x + y;
     }
 
 }
