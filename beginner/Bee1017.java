da912ace3d58da9fe73b1df6be972ff470ccf0fb
Marcos Vinícius Nunes <imarcosvinicius@outlook.com>
2025-04-30 07:45:26 -0300

    Fuel Consumption

---
+++ beginner/Bee1017.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Bee1017 {
+
+    private static final int AUTONOMY = 21;
+    public static void main(String[] args) {
+        try (Scanner sc = new Scanner(System.in)) {
+ 
+            int travelTime = sc.nextInt();
+            int avarageSpeed = sc.nextInt();
+
+            double distance = calculateDistance(avarageSpeed, travelTime);
+            double fuelSpent = calculateFuelSpent(distance);
+
+            System.out.printf("%.3f%n", fuelSpent);
+        }
+    }
+
+    private static double calculateDistance(int speed, int time) {
+        return speed * time;
+    }
+
+    private static double calculateFuelSpent(double distance) {
+        return distance / AUTONOMY;
+    }
+
+}
